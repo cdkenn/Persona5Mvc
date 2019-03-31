cc97a9fc6d4ca299a450f087b18e1ce26584fb38
Collin Kennedy <collin@icebergtech.org>
2019-03-31 10:51:46 -0600

    initial commit after project creation

---
+++ Persona5Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Persona5Mvc.Startup))]
+namespace Persona5Mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
